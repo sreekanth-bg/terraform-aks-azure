2aadd9382313c77f9f1b7ed3b3a7127520a9f997
Sreekanth Govindaiah <31440235+sreekanth-bg@users.noreply.github.com>
2021-12-14 18:12:34 +0530

    Update variables.tf
    azure login is provided via env vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,18 +7,18 @@ variable "location" {
   description = "The Azure Region in which all resources in this example should be provisioned"
 }
 
-variable "client_app_id" {
-  description = "The Client app ID of the AKS client application"
-}
+# variable "client_app_id" {
+#   description = "The Client app ID of the AKS client application"
+# }
 
-variable "server_app_id" {
-  description = "The Server app ID of  the AKS server application"
-}
+# variable "server_app_id" {
+#   description = "The Server app ID of  the AKS server application"
+# }
 
-variable "server_app_secret" {
-  description = "The secret created for AKS server application"
-}
+# variable "server_app_secret" {
+#   description = "The secret created for AKS server application"
+# }
 
-variable "tenant_id" {
-  description = "The Azure AD tenant id "
-}
+# variable "tenant_id" {
+#   description = "The Azure AD tenant id "
+# }
